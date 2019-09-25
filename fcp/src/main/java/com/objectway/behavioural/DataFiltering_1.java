dbe63bd879498c5f3a4741d9651d3564400881f2
Renato Perini <renato.perini@gmail.com>
2019-09-26 00:35:40 +0200

    First skeleton with a first example of Behavioural Parameterization Pattern

---
+++ fcp/src/main/java/com/objectway/behavioural/DataFiltering_1.java
@@ -0,0 +1,101 @@
+package com.objectway.behavioural;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.objectway.model.Apple;
+
+/**
+ * @author Renato Perini <renato.perini@objectway.com>
+ * Data Manipulation - Filtering list in an ingenuous way.
+ */
+public class DataFiltering_1 {
+
+	public static void main(String[] args) {
+		
+		List<Apple> inventory = new ArrayList<>();
+		
+		// Some apples. 
+		Apple apple1 = new Apple("red", 100);
+		Apple apple2 = new Apple("green", 150);
+	    Apple apple3 = new Apple("red", 180);
+	    Apple apple4 = new Apple("yellow", 130);
+	    
+	    // Let's put them in our inventory.
+	    inventory.add(apple1);
+	    inventory.add(apple2);
+	    inventory.add(apple3);
+	    inventory.add(apple4);
+	    
+	    // Let's have fun with simple filtering.
+	    List<Apple> redApples = filterRedApples(inventory);
+	    List<Apple> yellowApples = filterYellowApples(inventory);
+	    List<Apple> heavyApples = filterHeavyApples(inventory);
+	    
+	    // Let's check the result printing it out to the screen.
+	    printInventory(redApples);
+	    printInventory(yellowApples);
+	    printInventory(heavyApples);
+	}
+	
+	/**
+	 * Filter a list and returns a new list containing only red apples.
+	 * @param inventory - The original inventory for apples.
+	 * @return List<Apple> - A new inventory containing red apples.
+	 */
+	public static List<Apple> filterRedApples(List<Apple> inventory) {
+		List<Apple> result = new ArrayList<>();
+		for (Apple apple : inventory) {
+			if ("red".equalsIgnoreCase(apple.getColor())) {
+				result.add(apple);
+			}
+		}
+		
+		return result;
+	}
+	
+	/**
+	 * Filter a list and returns a new list containing only yellow apples.
+	 * @param inventory - The original inventory for apples.
+	 * @return List<Apple> - A new inventory containing yellow apples.
+	 */
+	public static List<Apple> filterYellowApples(List<Apple> inventory) {
+		List<Apple> result = new ArrayList<>();
+		for (Apple apple : inventory) {
+			if ("yellow".equalsIgnoreCase(apple.getColor())) {
+				result.add(apple);
+			}
+		}
+		
+		return result;
+	}
+	
+	/**
+	 * Filter a list and returns a new list containing apples that weight >= 130 grams.
+	 * @param inventory - The original inventory for apples.
+	 * @return List<Apple> - The resulting inventory
+	 */
+	public static List<Apple> filterHeavyApples(List<Apple> inventory) {
+		List<Apple> result = new ArrayList<>();
+		for (Apple apple : inventory) {
+			if (apple.getWeight() >= 130) {
+				result.add(apple);
+			}
+		}
+		
+		return result;
+	}
+    
+	/**
+	 * Just prints out the inventory content.
+	 * @param inventory - The inventory that we want to inspect
+	 */
+	public static void printInventory(List<Apple> inventory) {
+	    System.out.println("Inventory contains " + inventory.size() + " apples.");
+		for (int i = 0; i < inventory.size(); i++) {
+			Apple apple = inventory.get(i);
+			System.out.println("Apple " + i + ": " + "Color: " + apple.getColor() + " Weight: " + apple.getWeight());
+		}
+		System.out.println();
+	}
+}
---
+++ fcp/src/main/java/com/objectway/model/Apple.java
@@ -0,0 +1,32 @@
+package com.objectway.model;
+
+public class Apple {
+	
+	private String color;
+	private Integer weight;
+	
+	
+	public Apple() {}
+
+	public Apple(String color, Integer weight) {
+		this.color = color;
+		this.weight = weight;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public Integer getWeight() {
+		return weight;
+	}
+
+	public void setWeight(Integer weight) {
+		this.weight = weight;
+	}
+	
+}
